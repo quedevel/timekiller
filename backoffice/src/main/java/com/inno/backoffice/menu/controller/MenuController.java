fedf9973f44ed5b1878508da9fe3367a0060e9b5
quedevel <kdh90817@naver.com>
2021-02-11 20:38:46 +0900

    jstree.js 적용중

---
+++ backoffice/src/main/java/com/inno/backoffice/menu/controller/MenuController.java
@@ -0,0 +1,15 @@
+package com.inno.backoffice.menu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/menu")
+public class MenuController {
+
+    @GetMapping("/menuList")
+    public void menu(){
+
+    }
+}
