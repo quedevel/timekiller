bb4ccc4623600383c2f2b91023ff3bf0171b1d65
quedevel <kdh90817@naver.com>
2021-03-03 21:49:27 +0900

    AOP 추가 작업중

---
+++ backoffice/src/main/java/com/inno/backoffice/aop/SessionAspect.java
@@ -0,0 +1,11 @@
+package com.inno.backoffice.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class SessionAspect {
+
+    
+}
